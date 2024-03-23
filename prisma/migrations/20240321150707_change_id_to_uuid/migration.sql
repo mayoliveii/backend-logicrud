6cee91d7460ea86182a3125317fcad4256ca3ac8
mayoliveii <mayoliveirablv@gmail.com>
2024-03-23 15:16:10 -0300

    feat: create database migrations

---
+++ prisma/migrations/20240321150707_change_id_to_uuid/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Categories" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "Categories_pkey" PRIMARY KEY ("id")
+);
